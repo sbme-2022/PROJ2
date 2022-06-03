d3d0f48d976e6efca233f4f7377424e7106eaa2e
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-06-03 22:46:20 +0200

    init nodejs server

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_req, res) => res.send('Currency Converter PROJ2!'))
+app.listen(PORT, () => console.log(`server started at http://localhost:${PORT}`))
