47e70563bc5f65cebca351b3813c21920e4a1660
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-06-03 23:22:00 +0200

    Fetch Data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
+const axios = require('axios');
+
+
+headers = {"apikey": "kes6gUlcNf5ip1pjLhfgr2KI8CTfuaWN"};
+axios.get('https://api.apilayer.com/exchangerates_data/latest', {headers: headers})
+    .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    console.log((res.data));
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 app.get('/', (_req, res) => res.send('Currency Converter PROJ2!'))
 app.listen(PORT, () => console.log(`server started at http://localhost:${PORT}`))
