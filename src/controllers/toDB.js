412c9d5fcc77223709d235368993d55ef0e901f7
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2022-06-05 00:18:03 +0200

    Squashed commit of the following:
    commit 62c01e75c672015252da63f82ba41910b8a8a790
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Sat Jun 4 22:22:02 2022 +0200
    
        fix review comments
    
    commit 0616ecf594c1acba7e2dca2635a2708d31e2cf0b
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Sat Jun 4 01:22:40 2022 +0200
    
        FIX API request
    
    commit 8e4e6fc0e97dc873ac7eb635d735d5eb176e7eed
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Sat Jun 4 01:22:26 2022 +0200
    
        database startup
    
    commit ce8f136246de913c39dd07f96dcc76a21cf36c62
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Sat Jun 4 01:22:10 2022 +0200
    
        Fetch From API Unit Test
    
    commit 057d747d1ed519e38528f71756349d61366823ac
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:43:43 2022 +0200
    
        Create database.js
    
    commit 312fad55eaf2a80ea29752f927ad337606faf9f5
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:40:47 2022 +0200
    
        toDB controller
    
    commit 0245696aa00923b175443f66cbf8e37832f4ea25
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:40:22 2022 +0200
    
        rates Model
    
    commit 32ee4504d21beaac82fc7edd3f84d59fea181cec
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:40:14 2022 +0200
    
        API request util
    
    commit 1449bfa3e4bd65132b3ba8c4b32eb7f8cec2d5c9
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:39:58 2022 +0200
    
        Update index.js
    
    commit 47e70563bc5f65cebca351b3813c21920e4a1660
    Author: Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
    Date:   Fri Jun 3 23:22:00 2022 +0200
    
        Fetch Data

--- src/controllers/toDB.js
+++ src/controllers/toDB.js
@@ -1,15 +1,22 @@
-const API_request = require('./utils/API_Request');
-const {CurrencyRate, validate} = require('./models/rates');
+const API_request = require('../utils/API_Request');
+const { CurrencyRate, validate } = require('../models/rates');
 
 const fetch = async (_req, res) => {
     try {
         const data = await API_request();
-        const {base, date, rates} = data;
-        const currencyRate = new CurrencyRate({base, date, rates});
-        const {error} = validate(currencyRate);
+
+        if (!data) {
+            return res.status(400).send('API request failed');
+        }
+
+        const { base, date, rates } = data;
+        const currencyRate = new CurrencyRate({ base, date, rates });
+
+        const { error } = validate(currencyRate);
         if (error) {
             return res.status(400).send(error.details[0].message);
         }
+
         currencyRate.save();
         res.send(data);
     }
