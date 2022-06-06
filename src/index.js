c113d5d5e5a99db84d6bbf421ea0a0f45a43f42a
Mo'men Maged <momen99223@gmail.com>
2022-06-06 20:30:03 +0200

    fixed scheduler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const job = require("../startup/scheduler");
+const app = express();
 
-require('./startup/database')()
-require('./startup/routes')(app);
+require("./startup/database")();
+require("./startup/routes")(app);
 
-app.get('/', (_req, res) => res.send('Currency Converter PROJ2!'))
+app.get("/", (_req, res) => res.send("Currency Converter PROJ2!"));
+job();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`server started at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`server started at http://localhost:${PORT}`)
+);
