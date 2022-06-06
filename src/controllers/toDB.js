a8052a5bfe3807d08fe56734e090ca151464cd7a
Mo'men Maged <momen99223@gmail.com>
2022-06-06 18:35:51 +0200

    added scheduler

--- src/controllers/toDB.js
+++ src/controllers/toDB.js
@@ -1,30 +1,29 @@
-const API_request = require('../utils/API_Request');
-const { CurrencyRate, validate } = require('../models/rates');
-
-const fetch = async (_req, res) => {
+const API_request = require("../utils/API_Request");
+const { CurrencyRate, validate } = require("../models/rates");
+const schedule = require("node-schedule");
+const fetch_schedule = schedule.scheduleJob(
+  "* 6 * * *",
+  (fetch = async (_req, res) => {
     try {
-        const data = await API_request();
-        const { base, date, rates } = data;
-        const currencyRate = new CurrencyRate({ base, date, rates });
-        const { error } = validate(currencyRate);
+      const data = await API_request();
 
-        if(!data){
-            return res.status(400).send('API request failed');
-        }
+      if (!data) {
+        return res.status(400).send("API request failed");
+      }
 
-        const {base, date, rates} = data;
-        const currencyRate = new CurrencyRate({base, date, rates});
-        
-        const {error} = validate(currencyRate);
-        if (error) {
-            return res.status(400).send(error.details[0].message);
-        }
-        currencyRate.save();
-        res.send(data);
+      const { base, date, rates } = data;
+      const currencyRate = new CurrencyRate({ base, date, rates });
+
+      const { error } = validate(currencyRate);
+      if (error) {
+        return res.status(400).send(error.details[0].message);
+      }
+      currencyRate.save();
+      res.send(data);
+    } catch (err) {
+      res.status(400).send(err);
     }
-    catch (err) {
-        res.status(400).send(err);
-    }
-}
+  })
+);
 
-exports.fetch = fetch;
+exports.fetch_schedule = fetch_schedule;
